3da1286f60e1fb3cd096002ed7047125181454a3
sufiyaandev <137880325+sufiyaandev@users.noreply.github.com>
2024-05-16 16:06:40 +0530

    added email in schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -7,10 +7,15 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     password: {
       type: String,
       required: true,
-    },
+    }, 
   },
   { timestamps: true }
 );
