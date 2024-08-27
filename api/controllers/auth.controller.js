b5d5e55baa08c9a26330da9120c46be76caa7443
sufiyandev <sifuak99@gmail.com>
2024-08-27 16:01:58 +0530

    to control error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,6 +5,11 @@ export const signup = async (req, res) => {
   const { username, email, password } = req.body;
   const hashedPassword = bycryptjs.hashSync(password, 10);
   const newUser = new User({username,email,password: hashedPassword});
-  await newUser.save();
-  res.status(201).json('user created succesfully.!')
+  try {
+    await newUser.save();
+    res.status(201).json('user created succesfully.!')
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
+
 };
