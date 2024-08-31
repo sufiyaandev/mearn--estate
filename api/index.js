5d1bf27b0e06d4d40dc59d6cfecb38b4f61cdebb
sufiyandev <sifuak99@gmail.com>
2024-08-31 22:23:36 +0530

    created user update api

--- api/index.js
+++ api/index.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-console.log("MONGO URI:", process.env.MONGO);
+// console.log("MONGO URI:", process.env.MONGO);
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -20,19 +20,20 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is runing on port 3000");
 });
 app.use("/api/user", userRouter);
-app.use('/api/auth',authRouter);
+app.use("/api/auth", authRouter);
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-  })
-})
+  });
+});
