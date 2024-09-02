13a3fbc81cc4c5280d51ba1098324331d640ef0e
sufiyandev <sifuak99@gmail.com>
2024-09-02 17:27:40 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-
+import listingRouter from "./routes/listing.route.js"
 dotenv.config();
 
 // console.log("MONGO URI:", process.env.MONGO);
@@ -27,13 +27,15 @@ app.listen(3000, () => {
 });
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use('/api/listing',listingRouter);
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
-    statusCode,
+    statusCode, 
     message,
   });
 });
