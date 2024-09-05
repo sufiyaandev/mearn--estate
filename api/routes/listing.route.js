97b413aa87c871d5b1f4497d1bdd1c61bbfb6cb9
sufiyandev <sifuak99@gmail.com>
2024-09-05 14:47:50 +0530

    complete create listing page functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,21 @@
-import express from 'express';
-import { verifyToken } from '../utils/verifyUser.js';
-import { createListing } from '../controllers/listing.controller.js';
+// import express from 'express';
+// import { createListing } from '../controllers/listing.controller.js';
+// import { verifyToken } from '../utils/verifyUser.js';
+
+// const router = express.Router();
+
+// router.post('/create',verifyToken, createListing)
+
+// export default router;
+
+import express from "express";
+import { createListing } from "../controllers/listing.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post('/create',verifyToken, createListing)
+router.post("/create", 
+    // verifyToken, 
+    createListing);
 
 export default router;
- 
