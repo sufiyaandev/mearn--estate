a0d0bfcc2c9e1790685aedeb1eb96e5285275a90
sufiyandev <sifuak99@gmail.com>
2024-09-05 15:53:05 +0530

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -35,6 +36,8 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
+  console.log('req.user.id:', req.user.id);
+  console.log('req.params.id:', req.params.id);
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, "You can Only delete your own account!"));
   try {
@@ -42,6 +45,19 @@ export const deleteUser = async (req, res, next) => {
     res.clearCookie("access_token");
     res.status(200).json("User has been deleted!");
   } catch (error) {
-    next(error);
+    next(error); 
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings"));
   }
 };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -14,8 +14,6 @@ import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/create", 
-    // verifyToken, 
-    createListing);
+router.post("/create", verifyToken, createListing);
 
 export default router;
