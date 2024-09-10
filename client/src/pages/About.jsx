8760046033ebde54442d4bfa8c212e36f9a68d46
sufiyandev <sifuak99@gmail.com>
2024-09-10 22:19:53 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Buy Estate
+      </h1>
+      <p className="mb-4 text-slate-500">
+        Welcome to Buy Estate, your trusted partner in finding the perfect home
+        or investment property. We are dedicated to making the property search
+        and buying experience simple and hassle-free. Whether you're looking to
+        buy, rent, or invest, our platform provides a vast range of listings
+        across various categories, ensuring that you find the ideal place that
+        meets your needs and lifestyle. With user-friendly search tools and
+        detailed property information, we’re here to help you make informed
+        decisions with confidence.
+      </p>
+      <p className="mb-4 text-slate-500">
+        At Buy Estate, we believe that finding a home is more than just a
+        transaction—it's about building a future. Our team is committed to
+        offering personalized service, understanding your unique requirements,
+        and delivering tailored property recommendations. From modern apartments
+        and family homes to luxury villas and commercial spaces, we ensure you
+        have access to the latest listings in the market. Our expert advice and
+        support are available every step of the way, helping you navigate the
+        property market smoothly.
+      </p>
+      <p className="mb-4 text-slate-500">
+        We are more than just a property platform—we are a community. At Buy
+        Estate, we pride ourselves on fostering long-term relationships with our
+        clients, providing ongoing support even after you've made your purchase.
+        Whether you're a first-time homebuyer, a seasoned investor, or someone
+        looking to rent, our mission is to connect you with your dream property
+        and make your journey a seamless, enjoyable experience.
+      </p>
     </div>
-  )
+  );
 }
